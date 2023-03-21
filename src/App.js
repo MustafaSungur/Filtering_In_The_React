9c987e64804a2a3eb41cf2ef905060080e700729
Mustafa Sungur Öztürk <mustafasungur.ozturkk@gmail.com>
2023-03-21 16:13:16 +0300

    Finished

--- src/App.js
+++ src/App.js
@@ -2,46 +2,52 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const inputHandle = (e) => {
-    e.preventDefault();
-    console.log(e);
-    setTodos([...todos, todo]);
-    setTodo("");
+  const list = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Watermelon",
+  ];
+  const [filterList, setFilterLİst] = useState(list);
+
+  const changeHandle = (e) => {
+    if (e.target.value === "") {
+      setFilterLİst(list);
+      return;
+    }
+    const filterValues = list.filter((item) =>
+      item.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    setFilterLİst(filterValues);
   };
 
   return (
     <div className="w-10/12 mx-auto h-full absolute top-1/4 text-neutral-200  left-1/3 font-open">
-      {/* input */}
-
-      <form onSubmit={inputHandle} className="group flex">
+      <div className="group flex">
         <input
           className=" w-80  border-b-2 transition duration-300 border-y-red-700  outline-none  focus:border-b-emerald-600  bg-transparent"
-          placeholder="Ara / Ekle"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Search"
+          onChange={(e) => changeHandle(e)}
         />
-        <button
-          onClick={inputHandle}
-          className="ml-2 px-4 transition duration-300 bg-orange-600 rounded hover:bg-emerald-600"
-          disabled={!todo}
-        >
-          Ekle
-        </button>
-      </form>
+      </div>
+
       <ul
         className={
-          !todos === []
-            ? "border-none"
-            : "group space-y-2 mt-4 border w-fit p-6 rounded border-orange-500 text-zinc-400"
+          "group space-y-2 mt-4 border w-fit p-6 rounded border-orange-500 text-zinc-400"
         }
       >
-        {todos.map((data, index) => (
-          <li key={index} className=" list-item  ">
-            {index + 1}) {data}
-          </li>
-        ))}
+        {filterList.length === 0 ? (
+          <li className=" list-item  ">No result</li>
+        ) : (
+          filterList.map((data, index) => (
+            <li key={index} className=" list-item  ">
+              {index + 1}) {data}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
