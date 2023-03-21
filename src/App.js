a9a74365a0833c85bb02d10519a74c0a26fa5cfa
Mustafa Sungur Öztürk <mustafasungur.ozturkk@gmail.com>
2023-03-21 15:10:19 +0300

    it was added input and list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const inputHandle = (e) => {
+    e.preventDefault();
+    console.log(e);
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-10/12 mx-auto h-full absolute top-1/4 text-neutral-200  left-1/3 font-open">
+      {/* input */}
+
+      <form onSubmit={inputHandle} className="group flex">
+        <input
+          className=" w-80  border-b-2 transition duration-300 border-y-red-700  outline-none  focus:border-b-emerald-600  bg-transparent"
+          placeholder="Ara / Ekle"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button
+          onClick={inputHandle}
+          className="ml-2 px-4 transition duration-300 bg-orange-600 rounded hover:bg-emerald-600"
+          disabled={!todo}
         >
-          Learn React
-        </a>
-      </header>
+          Ekle
+        </button>
+      </form>
+      <ul
+        className={
+          !todos === []
+            ? "border-none"
+            : "group space-y-2 mt-4 border w-fit p-6 rounded border-orange-500 text-zinc-400"
+        }
+      >
+        {todos.map((data, index) => (
+          <li key={index} className=" list-item  ">
+            {index + 1}) {data}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
